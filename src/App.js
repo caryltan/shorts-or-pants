442e8edde420a8affcfae9db343de7b3b327855c
Caryl Tan <tan.caryl@gmail.com>
2023-02-03 17:13:14 -0500

    api call and displaying to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [weatherData, setWeatherData] = useState();
+
+  async function getWeatherData() {
+        const url = new URL('https://api.open-meteo.com/v1/forecast');
+        url.search = new URLSearchParams({
+          latitude: 52,
+          longitude: 52,
+          current_weather: true,
+        })    
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setWeatherData(json);
+  }
+
+  useEffect(() => {
+    getWeatherData();
+    console.log(weatherData);
+  }, []);
+  
   return (
     <div className="App">
+      <h1>Pants or Shorts</h1>
+      <h2>What to wear today based on the weather</h2>
+      
+      {weatherData &&  
+      <h3>{weatherData.current_weather.temperature}</h3>
+      }
     </div>
   );
 }
